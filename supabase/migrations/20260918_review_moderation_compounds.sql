bb23065352ac09bbc61392756899612884cd84e4
ninyorico <bancoronino6@gmail.com>
2026-09-18 22:25:02 +0800

    Block compound Tagalog review terms

---
+++ supabase/migrations/20260918_review_moderation_compounds.sql
@@ -0,0 +1,16 @@
+-- Catch common compound Tagalog forms used in inappropriate reviews.
+create or replace function public.contains_inappropriate_review_language(p_text text)
+returns boolean
+language sql
+immutable
+as $$
+  select coalesce(
+    lower(coalesce(p_text, '')) ~* '(^|[^[:alnum:]])(asshole|bastard|bitch|bullshit|cunt|dick|fuck|fucker|motherfucker|nigger|piss|porn|shit|slut|whore|putang[[:space:]]*ina|tangina|tanga|gago|gaga|napakagago|napakagaga|ulol|tarantado|leche|lecheng|pakyu|burat|kantot|iyot)([^[:alnum:]]|$)',
+    false
+  );
+$$;
+
+revoke all on function public.contains_inappropriate_review_language(text) from public;
+grant execute on function public.contains_inappropriate_review_language(text) to anon, authenticated;
+
+notify pgrst, 'reload schema';
